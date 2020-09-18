a0fd84125414919e38bdc96d43f6ffb314f2e089
Oleksandr Krysan <krolex@gmail.com>
2020-09-18 23:49:03 +0300

    Add JoinHintScope

---
+++ WaveAccess.Data.Entity/WaveAccess.Data.Entity.Test/Log4NetTestHelper.cs
@@ -0,0 +1,45 @@
+﻿using log4net;
+using log4net.Appender;
+using log4net.Config;
+using log4net.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace WaveAccess.Data.Entity.Test
+{
+    public static class Log4NetTestHelper
+    {
+        public static string[] RecordLog(Action action)
+        {
+            if (!LogManager.GetRepository().Configured)
+                BasicConfigurator.Configure();
+            var logMessages = new List<string>();
+            var root = ((log4net.Repository.Hierarchy.Hierarchy)LogManager.GetRepository()).Root;
+            var attachable = root as IAppenderAttachable;
+
+            var appender = new MemoryAppender();
+            if (attachable != null)
+                attachable.AddAppender(appender);
+
+            try
+            {
+                action();
+            }
+            finally
+            {
+                var loggingEvents = appender.GetEvents();
+                foreach (var loggingEvent in loggingEvents)
+                {
+                    var stringWriter = new StringWriter();
+                    loggingEvent.WriteRenderedMessage(stringWriter);
+                    logMessages.Add(string.Format("{0} - {1} | {2}", loggingEvent.Level.DisplayName, loggingEvent.LoggerName, stringWriter.ToString()));
+                }
+                if (attachable != null)
+                    attachable.RemoveAppender(appender);
+            }
+
+            return logMessages.ToArray();
+        }
+    }
+}
--- WaveAccess.Data.Entity/WaveAccess.Data.Entity.Test/MultiContextTest.cs
+++ WaveAccess.Data.Entity/WaveAccess.Data.Entity.Test/MultiContextTest.cs
@@ -6,6 +6,7 @@ using WaveAccess.Data.Entity.Test.Module2.Models;
 using WaveAccess.Data.Entity.Test.Module1.Expressions;
 using WaveAccess.Data.Entity.Hint;
 using System.Data.Entity.Infrastructure.Interception;
+using log4net;
 
 namespace WaveAccess.Data.Entity.Test {
 
@@ -13,9 +14,10 @@ namespace WaveAccess.Data.Entity.Test {
     public class MultiContextTest
     {
         [TestMethod]
-        public void TestTasks() {
+        public void TestTasks()
+        {
             DbInterception.Add(new HintInterceptor());
-            using (var userContext = new UserContext())
+             using (var userContext = new UserContext())
             {
                 userContext.Database.Initialize(true);
             }
@@ -23,7 +25,9 @@ namespace WaveAccess.Data.Entity.Test {
             {
                 test.Database.Initialize(true);
             }
-            using (var db = new GenericContext()) {
+
+            using (var db = new GenericContext())
+            {
                 using (var qh = new HintScope("HASH JOIN"))
                 {
                     var tasks = db.Set<Task>().Include(t => t.User).Where(t => db.Set<User>().
@@ -33,6 +37,34 @@ namespace WaveAccess.Data.Entity.Test {
                     Assert.AreEqual(4, testarr.Length);
                 }
             }
+        }
+             [TestMethod]
+        public void TestScope()
+        {
+
+            using (var userContext = new UserContext())
+            {
+                userContext.Database.Initialize(true);
+            }
+            using (var test = new TaskContext())
+            {
+                test.Database.Initialize(true);
+            }
+            DbInterception.Add(new HintInterceptor());
+            DbInterception.Add(new Log4NetCommandInterceptor());
+            var testing = Log4NetTestHelper.RecordLog(() =>
+            {
+                using (var db = new GenericContext())
+                {
+                    using (var qh = new JoinHintScope<GroupHierarchy>("HASH"))
+                    {
+                        var tasks = db.Set<Task>().Include(t => t.User).Where(t => db.Set<User>().
+                          Where(UserExpressions.UserByGroups(1)).Any(u => u.Id == t.UserId));
+                        var testarr = tasks.ToArray();
+                    }
+                }
+            });                                
+            Assert.IsTrue(testing[0].Contains("INNER HASH JOIN [dbo].[v_GroupHierarchy]"));
         }
     }
 }
