a7a12182ea32128a46b2c81a2cec9f2278d83ff1
Oleksandr V. Krysan <krolex@gmail.com>
2020-09-18 23:19:19 +0300

    Add JoinHintScope

---
+++ WaveAccess.Data.Entity/WaveAccess.Data.Entity/DbContextMetaData.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.Entity.Core.Mapping;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.Entity.Core.Objects;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaveAccess.Data.Entity
+{
+    static class DbContextMetaData
+    {
+        public static string GetTableName<T>(this DbContext context) where T : class
+        {
+            ObjectContext objectContext = ((IObjectContextAdapter)context).ObjectContext;
+            return objectContext.GetTableName(typeof(T));
+        }
+
+        public static string GetTableName(this DbContext context, Type t)
+        {
+            ObjectContext objectContext = ((IObjectContextAdapter)context).ObjectContext;
+            return objectContext.GetTableName(t);
+        }
+
+        public static string GetTableName<T>(this ObjectContext context) where T : class
+        {
+            return context.GetTableName(typeof(T));
+        }
+        public static string GetTableName(this ObjectContext context, Type t)
+        {
+            string entityName = t.Name;
+            ReadOnlyCollection<EntityContainerMapping> storageMetadata = context.MetadataWorkspace.GetItems<EntityContainerMapping>(DataSpace.CSSpace);
+
+            foreach (EntityContainerMapping ecm in storageMetadata)
+            {
+                if (ecm.StoreEntityContainer.TryGetEntitySetByName(entityName, true, out EntitySet entitySet))
+                {
+                    return $"[{entitySet.Schema}].[{entitySet.Table}]";
+                }
+            }
+            throw new NotSupportedException($"Metadat for {t.Name} is not found");
+        }
+    }
+}
---
+++ WaveAccess.Data.Entity/WaveAccess.Data.Entity/Hint/JoinHintScope.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Data.Entity.Infrastructure.Interception;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WaveAccess.Data.Entity.Hint
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T">Type of Entity </typeparam>
+    public class JoinHintScope<T> : HintScope where T : class
+    {
+        public JoinHintScope(string hint):base(hint)
+        {
+
+        }
+
+        protected internal override void ApplyHint<TContext>(DbCommand command, DbCommandInterceptionContext<TContext> interceptionContext)
+        {
+            var oc = interceptionContext.ObjectContexts.First();
+            var tableName = oc.GetTableName<T>();
+            command.CommandText = Regex.Replace(command.CommandText,  $"JOIN\\s+{Regex.Escape(tableName)}", $"{QueryHint} JOIN {tableName}",
+                                      RegexOptions.IgnoreCase|RegexOptions.Multiline);
+        }
+    }
+}
