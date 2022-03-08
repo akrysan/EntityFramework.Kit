7ce8e3184bfe12ea196e699af5a706c12c59e36d
Yaroslav Shparuk <46866330+yaroslavshparuk@users.noreply.github.com>
2022-03-08 14:07:20 +0200

    Update EF version to 6.4.4 and bump version to 2.3.0

--- WaveAccess.Data.Entity/WaveAccess.Data.Entity/Properties/AssemblyInfo.cs
+++ WaveAccess.Data.Entity/WaveAccess.Data.Entity/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("WaveAccess.Data.Entity")]
 [assembly: AssemblyDescription(@"A collection of EF contributions for .NET Entity Framework 6")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("WaveAccess")]
+[assembly: AssemblyCompany("Aptem")]
 [assembly: AssemblyProduct("WaveAccess.Data.Entity")]
-[assembly: AssemblyCopyright("© WaveAccess")]
+[assembly: AssemblyCopyright("© Aptem")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.2.0.0")]
-[assembly: AssemblyFileVersion("2.2.0.0")]
+[assembly: AssemblyVersion("2.3.0.0")]
+[assembly: AssemblyFileVersion("2.3.0.0")]
