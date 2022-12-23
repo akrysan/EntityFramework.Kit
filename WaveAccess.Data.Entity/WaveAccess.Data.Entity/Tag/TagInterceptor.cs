106ad88d30168cea0c623be2244324db3f8d34ac
Sergii Liesnoi <sergiil@mwstechnology.com>
2022-12-23 17:18:10 +0200

    Tag interceptor was added

---
+++ WaveAccess.Data.Entity/WaveAccess.Data.Entity/Tag/TagInterceptor.cs
@@ -0,0 +1,29 @@
+﻿namespace WaveAccess.Data.Entity.Tag {
+    using System.Data.Common;
+    using System.Data.Entity.Infrastructure.Interception;
+    public class TagInterceptor : DbCommandInterceptor {
+        public override void ReaderExecuting(DbCommand command, DbCommandInterceptionContext<DbDataReader> interceptionContext) {
+            if (TagScope.CurrentTagScope != null) {
+                TagScope.CurrentTagScope.ApplyTag(command);
+            }
+
+            base.ReaderExecuting(command, interceptionContext);
+        }
+
+        public override void ScalarExecuting(DbCommand command, DbCommandInterceptionContext<object> interceptionContext) {
+            if (TagScope.CurrentTagScope != null) {
+                TagScope.CurrentTagScope.ApplyTag(command);
+            }
+
+            base.ScalarExecuting(command, interceptionContext);
+        }
+
+        public override void ScalarExecuted(DbCommand command, DbCommandInterceptionContext<object> interceptionContext) {
+            if (TagScope.CurrentTagScope != null) {
+                TagScope.CurrentTagScope.ApplyTag(command);
+            }
+
+            base.ScalarExecuted(command, interceptionContext);
+        }
+    }
+}
