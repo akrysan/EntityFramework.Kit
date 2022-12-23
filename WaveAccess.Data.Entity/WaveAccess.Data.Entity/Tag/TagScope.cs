083524058ab90f10252c28e85e89591d4650842c
SergiiLV <121303387+SergiiLV@users.noreply.github.com>
2022-12-23 18:19:19 +0200

    Tag interceptor was added (#10)
    Co-authored-by: Sergii Liesnoi <sergiil@mwstechnology.com>

---
+++ WaveAccess.Data.Entity/WaveAccess.Data.Entity/Tag/TagScope.cs
@@ -0,0 +1,21 @@
+﻿namespace WaveAccess.Data.Entity.Tag {
+    using System;
+    using System.Data.Common;
+    public class TagScope : IDisposable {
+        [ThreadStatic]
+        internal static TagScope CurrentTagScope;
+
+        public string QueryTag { get; private set; }
+
+        public void Dispose() => CurrentTagScope = null;
+
+        public TagScope(string tag) {
+            QueryTag = !string.IsNullOrWhiteSpace(tag) ? tag : throw new ArgumentOutOfRangeException(nameof(tag));
+            CurrentTagScope = this;
+        }
+
+        protected internal virtual void ApplyTag(DbCommand command) {
+            command.CommandText = string.Format("/*{0}*/ ", QueryTag) + command.CommandText;
+        }
+    }
+}
